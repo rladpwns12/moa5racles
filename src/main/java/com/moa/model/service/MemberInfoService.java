a6839aba0e67c2fb5bdb732836526fa523dacb07
kimyonghee <yhk06262@naver.com>
2019-08-12 17:51:58 +0900

    [김용희] 로그인 controller model 추가

---
+++ src/main/java/com/moa/model/service/MemberInfoService.java
@@ -0,0 +1,19 @@
+package com.moa.model.service;
+
+
+import com.moa.model.dao.UserDAO;
+import com.moa.model.vo.LoginVO;
+import com.moa.model.vo.SimpleUserInfoVO;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+public interface MemberInfoService {
+    public boolean checkExistUser(String userNick);
+    public SimpleUserInfoVO selectMemberInfo(int userId);
+    public boolean signUpMember(Map<String, Object> userInfo);
+    public boolean signUpDuplicationCheck(Map<String, Object> duplicationInfo);
+    public LoginVO memberLogin(Map<String, Object> loginInfo);
+}
