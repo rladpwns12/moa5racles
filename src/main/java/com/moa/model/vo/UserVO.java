5e0b188d2a0bd8f50b94ad976ca3b96cc0fbd6b3
chlalstjd <chlalstjd430@naver.com>
2019-08-12 22:04:09 +0900

    [회원가입 기능] - UserVO 생성

---
+++ src/main/java/com/moa/model/vo/UserVO.java
@@ -0,0 +1,14 @@
+package com.moa.model.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class UserVO {
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String nick;
+    private String name;
+}
