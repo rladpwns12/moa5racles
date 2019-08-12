527237cd79a731b69a8b4de71434737b893e8386
chlalstjd <chlalstjd430@naver.com>
2019-08-12 16:49:41 +0900

    [로그인 기능] - UserMapper Interface 구현

--- src/main/java/com/moa/mybatis/UserMapper.java
+++ src/main/java/com/moa/mybatis/UserMapper.java
@@ -2,8 +2,14 @@ package com.moa.mybatis;
 
 import com.moa.model.vo.SimpleUserInfoVO;
 
+import java.util.Map;
+
 
 public interface UserMapper {
     SimpleUserInfoVO selectUserInfo(int userId);
     int selectUserExist(String userNick);
+
+    int checkLogin(Map<String,Object> loginInfo);
+    int signupUser(Map<String,Object> userInfo);
+    int duplicationCheck(Map<String,Object> duplicationInfo);
 }
