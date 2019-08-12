7404807746847a153827286ad71b4d93eaeec8cf
chlalstjd <chlalstjd430@naver.com>
2019-08-12 16:56:38 +0900

    [로그인 기능] - UserMapper Interface 구현 수정

--- src/main/java/com/moa/mybatis/UserMapper.java
+++ src/main/java/com/moa/mybatis/UserMapper.java
@@ -9,7 +9,17 @@ public interface UserMapper {
     SimpleUserInfoVO selectUserInfo(int userId);
     int selectUserExist(String userNick);
 
-    int checkLogin(Map<String,Object> loginInfo);
     int signupUser(Map<String,Object> userInfo);
     int duplicationCheck(Map<String,Object> duplicationInfo);
+
+//    signup Map 정보
+//    - "userVO", userVO
+//    - "addressVo", addressVO
+//
+//    duplicationCheck 정보
+//    > 아래에서 검사할 것만 map에 담아서 보내주시면 됩니다.
+//    - "userEmail", userEmail(String)
+//    - "password", password(String)
+//    - "userNick", userEmail(String)
+
 }
