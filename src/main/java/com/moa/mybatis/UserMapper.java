9ec7e7cd357168b7c744dba41b96e2e364d38cd2
chlalstjd <chlalstjd430@naver.com>
2019-08-12 22:03:48 +0900

    [회원가입 기능] - 회원가입 Mapper Interface

--- src/main/java/com/moa/mybatis/UserMapper.java
+++ src/main/java/com/moa/mybatis/UserMapper.java
@@ -9,22 +9,7 @@ import java.util.Map;
 public interface UserMapper {
     SimpleUserInfoVO selectUserInfo(int userId);
     int selectUserExist(String userNick);
-    int signUpUser(Map<String,Object> userInfo);
+    void signUpUser(Map<String,Object> userInfo);
     int duplicationCheck(Map<String,Object> duplicationInfo);
     LoginVO checkLogin(Map<String,Object> loginInfo);
-
-//    checkLogin
-//    - "email", email
-//    - "password", password
-//
-//    signup Map 정보
-//    - "userVO", userVO
-//    - "addressVo", addressVO
-//
-//    duplicationCheck 정보
-//    - 아래에서 검사할 것만 map에 담아서 보내주시면 됩니다.
-//    - "email", userEmail(String)
-//    - "nick", password(String)
-
-
 }
