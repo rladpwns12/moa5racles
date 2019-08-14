a0aa42c4256160ed4315378112fe1122c97e49ed
JoonHee <mornings9047@naver.com>
2019-08-14 22:02:49 +0900

    [ISSUE-28] 송준희 - controller registrationForm -> registration으로 변경 + MemberInfo 관련된거??는 뭔지 모르겠음

--- src/main/java/com/moa/controller/LoginController.java
+++ src/main/java/com/moa/controller/LoginController.java
@@ -1,6 +1,10 @@
 package com.moa.controller;
 
+import com.moa.model.service.MemberInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
