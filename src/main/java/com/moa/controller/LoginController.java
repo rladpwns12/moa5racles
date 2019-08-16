623f35aecf390fc88e60f0fd12526dd6d758de00
김예준 <rladpwns12@naver.com>
2019-08-16 22:00:33 +0900

    [LO-1] Map형식 전달인자 Validate helper 클래스 구현

--- src/main/java/com/moa/controller/LoginController.java
+++ src/main/java/com/moa/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.moa.model.service.MemberRegistService;
 import com.moa.model.service.UserUpdateService;
 import com.moa.model.vo.AddressVO;
 import com.moa.model.vo.UserVO;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@Log4j
 public class LoginController {
     private static final String SUCCESS = "success";
     private static final String FAIL = "fail";
@@ -35,12 +37,12 @@ public class LoginController {
 
     @RequestMapping(value="/login")
     public String loginPage(String error, String logout, Model model){
-        System.out.println("loginPage()...");
+        log.info("loginPage()...");
         return "login";
     }
     @RequestMapping(value="/exit")
     public String exitRedirect(String error, String logout, Model model){
-        System.out.println("exitRedirect()...");
+        log.info("exitRedirect()...");
         return "exitRedirect";
     }
 
