2c16d476515a8e718f780c3059ca09d1ae6e13f2
JoonHee <mornings9047@naver.com>
2019-08-19 16:09:34 +0900

    [ISSUE-125] 송준희 - 리뷰  - 리뷰 유효성 검사 완료  - 리뷰 글 작성하기 완료

--- src/main/java/com/moa/model/vo/ReplyVO.java
+++ src/main/java/com/moa/model/vo/ReplyVO.java
@@ -2,12 +2,22 @@ package com.moa.model.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 public class ReplyVO {
+    @NotBlank
+    @Size(min = 1, max = 1000)
     private String comment;
+    @NotBlank
+    @Size(min = 1, max = 5)
     private int rating;
+    @NotBlank
     private int articleNum;
+    @NotBlank
     private int parentNum;
+    @NotBlank
     private int userId;
 
     @Override
