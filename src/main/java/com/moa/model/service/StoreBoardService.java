3551ea7ae9993bbddea1b2fe3db260c447f1f7b1
rladpwns12 <44272452+rladpwns12@users.noreply.github.com>
2019-08-21 17:22:31 +0900

    Update StoreBoardService.java

--- src/main/java/com/moa/model/service/StoreBoardService.java
+++ src/main/java/com/moa/model/service/StoreBoardService.java
@@ -1,9 +1,5 @@
 package com.moa.model.service;
 
-import com.moa.model.vo.DetailOptionVO;
-import com.moa.model.vo.EntrustSearchVO;
-
-import java.util.List;
 import java.util.Map;
 
 public interface StoreBoardService {
