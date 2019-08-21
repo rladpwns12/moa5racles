9b09b1691f15a29dae86c703d0f53c2e390d88a8
JoonHee <mornings9047@naver.com>
2019-08-22 00:07:00 +0900

    [ISSUE-259] 결제 코드 수정  - card, kakao Controller 통합

--- src/main/java/com/moa/controller/PayController.java
+++ src/main/java/com/moa/controller/PayController.java
@@ -2,28 +2,31 @@ package com.moa.controller;
 
 import com.moa.model.service.PayService;
 import com.moa.model.vo.PayVO;
-import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Log4j
 @Controller
-@RequestMapping(value = "/pay")
 public class PayController {
     @Autowired
     private PayService payService;
 
-    @RequestMapping(value = "/card", method = RequestMethod.GET)
-    public boolean card(@RequestParam int price) {
-
-
-        return false;
-    }
-
-    @RequestMapping(value = "/kakao", method = RequestMethod.POST)
+    @RequestMapping(value = "/pay")
     public @ResponseBody
-    boolean kakao(@RequestBody PayVO payVO) {
+    boolean pay(@RequestBody PayVO payVO) {
         return payService.updateHistory(payVO);
     }
+
+
+//    @RequestMapping(value = "/card", method = RequestMethod.GET)
+//    public @ResponseBody
+//    boolean card(@RequestBody PayVO payVO) {
+//        return payService.updateHistory(payVO);
+//    }
+//
+//    @RequestMapping(value = "/kakao", method = RequestMethod.POST)
+//    public @ResponseBody
+//    boolean kakao(@RequestBody PayVO payVO) {
+//        return payService.updateHistory(payVO);
+//    }
 }
