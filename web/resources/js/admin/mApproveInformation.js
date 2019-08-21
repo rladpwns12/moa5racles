faa49f91577c288ccd72c3572608af31f08ef8d2
choiminseong <cms3136@gmail.com>
2019-08-21 09:43:07 +0900

    [ISSUE-220] 관리자 호스트 인증 상세정보 디버깅
    - admin 호스트 인증 관리 거절/승인 이후 이동하는 페이지 URL변경
    - admin 호스트 거절시 alert창 제거
    - admin 호스트 승인시 승인여부 묻는 confirm창 띄어주기.

--- web/resources/js/admin/mApproveInformation.js
+++ web/resources/js/admin/mApproveInformation.js
@@ -2,22 +2,24 @@ var token = $("meta[name='_csrf']").attr("content");
 var header = $("meta[name='_csrf_header']").attr("content");
 
 
-function refuse(userId,url) {
-    alert(url);
+function refuse(userId) {
     var context = $('#content').val();
     //validation
     if(context == '' || context == null){
         alert('반드시 거절 사유를 적어주시기 바랍니다.');
         return;
     }
+    if(!confirm("신청을 거절하시겠습니까?")){
+        return;
+    }
     //start of ajax
     $.ajax({
-        url:"/admin/"+url+"/refuse?userId="+userId+"&context="+context,
+        url:"/admin/hostapprove/refuse?userId="+userId+"&context="+context,
         dataType:'json',
         success:function(result,url){
             if(result == true){
                 alert("승인이 거절되었습니다.");
-                location.href='/admin/'+url+'/list';
+                location.href='/admin/hostapprove/list';
                 return;
             }
             else{
@@ -31,21 +33,24 @@ function refuse(userId,url) {
 }
 
 
-function approve(userId,url) {
+function approve(userId) {
     console.log(userId);
     var context = $('#content').val();
     //validation
     if(context == '' || context == null){
         context  = '특이사항 없음.';
     }
+    if(!confirm("신청을 승인하시겠습니까?")){
+        return;
+    }
     //start of ajax
     $.ajax({
-        url:"/admin/"+url+"/confirm?userId="+userId+"&context="+context,
+        url:"/admin/hostapprove/confirm?userId="+userId+"&context="+context,
         dataType:'json',
         success:function(result){
             if(result == true){
                 alert("승인이 되었습니다.");
-                location.href='/admin/'+url+'/list';
+                location.href='/admin/hostapprove/list';
                 return;
             }
             else{
