284e241f499cb35b5114c364e75946f76ed80d67
김예준 <rladpwns12@naver.com>
2019-08-24 16:20:23 +0900

    s

--- web/resources/js/entrust.js
+++ web/resources/js/entrust.js
@@ -33,7 +33,7 @@ $(function () {
             str += "<input type='hidden' name='attachList[" + i + "].fileName' value='" + jobj.data("filename") + "'>";
             str += "<input type='hidden' name='attachList[" + i + "].uuid' value='" + jobj.data("uuid") + "'>";
             str += "<input type='hidden' name='attachList[" + i + "].uploadPath' value='" + jobj.data("path") + "'>";
-            str += "<input type='hidden' name='attachList[" + i + "].filetype' value='" + jobj.data("type") + "'>";
+            str += "<input type='hidden' name='attachList[" + i + "].fileType' value='" + jobj.data("type") + "'>";
         });
         formObjClone.append(str);
 
